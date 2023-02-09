fc0156341f08e9115616ca8becfa660d6e21079b
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-02-09 19:55:17 +0300

    Initial commit

---
+++ stalingrad/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from main_page import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='main_page')
+]
